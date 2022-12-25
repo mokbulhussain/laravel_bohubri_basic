bd77b9b748416fd9bedfa0a97d9140af4f8b9698
mokbulhussain <mokbulhussain2@gmail.com>
2022-12-25 13:51:55 +0600

    view generic

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/home', function(){
+//     return 'homepage';
+// });
+
+Route::view('/home', 'home');
