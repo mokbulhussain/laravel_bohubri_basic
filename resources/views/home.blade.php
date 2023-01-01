629f1233c7e11127392618ac90c6501886521a6a
mokbulhussain <mokbulhussain2@gmail.com>
2023-01-01 17:19:59 +0600

    external file and css and image link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
+  @extends('layout.custom')
+  @section('content')
     <h1>Home page by route with view</h1>
     @foreach ($product as $pro)
         @if ($pro=='pens')
@@ -18,5 +10,5 @@
                 {{'not find'}}
         @endif
     @endforeach
-</body>
-</html>
+
+@endsection
